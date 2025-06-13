8a013dd284d136d066525fdcd49034c4503f8a6c
Jeery1 <1705077827@qq.com>
2025-06-13 15:35:34 +0800

    将AI写作模块导入

---
+++ ui/ai_page.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTextEdit, QPushButton, QHBoxLayout
+class AIResultDialog(QDialog):
+    def __init__(self, result_text, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("AI 写作结果")
+        self.resize(600, 400)
+
+        layout = QVBoxLayout(self)
+
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setReadOnly(True)
+        self.text_edit.setPlainText(result_text)
+        layout.addWidget(self.text_edit)
+
+        btn_layout = QHBoxLayout()
+        self.copy_btn = QPushButton("复制文本", self)
+        self.copy_btn.clicked.connect(self.copy_text)
+        self.close_btn = QPushButton("关闭", self)
+        self.close_btn.clicked.connect(self.accept)
+        btn_layout.addWidget(self.copy_btn)
+        btn_layout.addStretch()
+        btn_layout.addWidget(self.close_btn)
+
+        layout.addLayout(btn_layout)
+
+    def copy_text(self):
+        clipboard = self.text_edit.clipboard()
+        clipboard.setText(self.text_edit.toPlainText())
